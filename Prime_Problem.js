0990db8d7b614ddd3efcdb4abc49d8c2595110d3
rakhi soni <yashvardhansoni7742@gmail.com>
2022-11-02 11:52:56 +0530

    Prime_No.

---
+++ Prime_Problem.js
@@ -0,0 +1,17 @@
+let num =13;
+    let c=0;
+for (let i=1;i<=num;i++)
+{
+    if (i%num==0)
+    {
+        c++;
+    }
+}
+if (c == 1)
+{
+    console.log("Yes")
+}
+else
+{
+    console.log("No")
+}
